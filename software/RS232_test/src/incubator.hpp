26d1fda3e921e1ecc60308ab6921c6df3886e2a5
Patrick Hanner <hannerpatrick@gmail.com>
2021-11-05 16:20:00 +0100

    finished schematic and added board

--- software/RS232_test/src/incubator.hpp
+++ software/RS232_test/src/incubator.hpp
@@ -5,6 +5,8 @@
 #include <thread>
 #include <iterator>
 
+#include "package.hpp"
+
 template <typename Serial>
 struct Incubator
 {
@@ -13,32 +15,7 @@ struct Incubator
   using clock_t = std::chrono::steady_clock;
   std::vector<std::byte> _fifo;
   static constexpr std::size_t MaxPackageSize{50};
-    struct Package
-  {
-    std::byte firstByte;
-    std::vector<std::byte> data;
-    unsigned int targetValue() const
-    {
-      return std::to_integer<unsigned int>(data[1]) << 8 |
-             std::to_integer<unsigned int>(data[0]);
-    }
-    std::int32_t currentValue() const
-    {
-      return std::to_integer<int32_t>(data[5]) << 24 |
-             std::to_integer<int32_t>(data[4]) << 16 |
-             std::to_integer<int32_t>(data[3]) << 8 |
-             std::to_integer<int32_t>(data[2]);
-    }
-
-    std::uint8_t dataLength() const
-    {
-      return std::to_integer<std::uint8_t>(firstByte) & 0x0F;
-    }
-    std::uint8_t command() const
-    {
-      return std::to_integer<std::uint8_t>(firstByte) & 0xF0;
-    }
-  };
+    
 
   template <std::size_t N>
   std::optional<std::array<std::byte, N>> tryReceive()
@@ -89,7 +66,6 @@ struct Incubator
 
   std::optional<Package> parse(std::vector<std::byte> &fifo)
   {
-    //Declare Lambda
     auto clearTillNextStartByte = [&](std::size_t skip)
     {
       auto iterBegin =
---
+++ software/RS232_test/src/package.hpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <cstddef>
+
+struct Package
+  {
+    std::byte firstByte;
+    std::vector<std::byte> data;
+    unsigned int targetValue() const
+    {
+      return std::to_integer<unsigned int>(data[1]) << 8 |
+             std::to_integer<unsigned int>(data[0]);
+    }
+    std::int32_t currentValue() const
+    {
+      return std::to_integer<int32_t>(data[5]) << 24 |
+             std::to_integer<int32_t>(data[4]) << 16 |
+             std::to_integer<int32_t>(data[3]) << 8 |
+             std::to_integer<int32_t>(data[2]);
+    }
+
+    std::uint8_t dataLength() const
+    {
+      return std::to_integer<std::uint8_t>(firstByte) & 0x0F;
+    }
+    std::uint8_t command() const
+    {
+      return std::to_integer<std::uint8_t>(firstByte) & 0xF0;
+    }
+  };
